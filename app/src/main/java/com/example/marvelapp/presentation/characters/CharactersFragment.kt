8e5b848c12745ec1ad1b04ac616ba4cf5ca29865
Leandro Cabral <leandrocabralf@gmail.com>
2022-08-23 17:55:54 -0300

    Camada de apresentação - Adicionando apresentação de lista de item de personagem

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,16 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.marvelapp.R
+import com.example.core.domain.model.Character
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val characterAdapter = CharacterAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
 
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharacterAdapter()
+
+        characterAdapter.submitList(
+            listOf(
+                Character(
+                    "3D-Man",
+                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+                ),
+                Character(
+                    "3D-Man",
+                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+                ),
+                Character(
+                    "3D-Man",
+                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+                ),
+                Character(
+                    "3D-Man",
+                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+                )
+            )
+        )
+    }
+
+    private fun initCharacterAdapter() {
+        with(binding.recyclerCharacter) {
+            setHasFixedSize(true)
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
 }
---
+++ core/src/main/java/com/example/core/domain/model/Character.kt
@@ -0,0 +1,6 @@
+package com.example.core.domain.model
+
+data class Character(
+    var name: String,
+    val imageUrl: String
+)
